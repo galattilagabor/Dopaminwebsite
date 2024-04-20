8cbe1a3391008d06e996682a23a911d38200a818
Fanni Gondos <fanni.gondos@stud.uni-corvinus.hu>
2024-04-20 12:45:29 +0200

    Add project files.

---
+++ ApiSample/Raktar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kliensalkalmazas
+{
+    public partial class Raktar : Form
+    {
+        public Raktar()
+        {
+            InitializeComponent();
+        }
+    }
+}
