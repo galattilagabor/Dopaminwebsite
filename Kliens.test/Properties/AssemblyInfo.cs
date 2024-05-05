2ace5d9a9c9e3253051d9c296439d91782d98486
Attila Gábor Gál <attila.gal2@stud.uni-corvinus.hu>
2024-05-05 16:35:33 +0200

    terméklista ürességének ellenőrzése, akadály a protection level-ben

---
+++ Kliens.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kliens.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kliens.test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf618140-77b9-4db3-b855-2babd9360841")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Kliens.test/Test_module1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kliensalkalmazas;
+using NUnit.Framework;
+
+namespace Kliens.test
+{
+    [TestFixture]
+    public class RaktarTests
+    {
+        public Raktár raktarForm;
+
+        [SetUp]
+        public void SetUp()
+        {
+            raktarForm = new Raktár();
+        }
+
+        [Test]
+        public void TermeklistaNemUres()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.That(raktarForm.termeklista, Is.Not.Empty);
+        }
+
+        // További tesztesetek...
+
+    }
+}
