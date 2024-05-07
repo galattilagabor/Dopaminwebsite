1191644c83117c43b4270da5aeb433b461412a3b
Attila Gábor Gál <attila.gal2@stud.uni-corvinus.hu>
2024-05-07 16:44:46 +0200

    működő verzió, a cél, hogy ellenőrizzük a szövegek formátumát (string)

--- Kliens.test/Test_module1.cs
+++ Kliens.test/Test_module1.cs
@@ -34,59 +34,17 @@ namespace Kliens.test
         }
 
         [Test]
-        public void Test_OpenMennyisegSzerkesztesForm()
+        public void Test_TextBoxContent_IsString()
         {
-            // Gomb lenyomása a fő formon
-            mainForm.Invoke(new Action(() =>
+            // Végigmegyünk minden TextBox-en a formon
+            foreach (Control control in mainForm.Controls)
             {
-                mainForm.button_change_quantity.PerformClick();
-            }));
-
-            // Megkeressük a megnyílt MennyiségSzerkesztés formot
-            MennyiségSzerkesztés mennyiségSzerkesztésForm = null;
-            foreach (Form form in Application.OpenForms)
-            {
-                if (form is MennyiségSzerkesztés && form.Visible)
+                if (control is TextBox textBox)
                 {
-                    mennyiségSzerkesztésForm = (MennyiségSzerkesztés)form;
-                    break;
+                    // Ellenőrizzük, hogy a TextBox tartalma string
+                    Assert.That(textBox.Text, Is.TypeOf<string>());
                 }
             }
-
-            // Ellenőrizzük, hogy a MennyiségSzerkesztés form bezáródott-e és DialogResult.OK lett-e
-            Assert.Multiple(() =>
-            {
-                Assert.That(mennyiségSzerkesztésForm, Is.Not.Null, "A MennyiségSzerkesztés form nem nyílt meg.");
-                Assert.That(mennyiségSzerkesztésForm.DialogResult, Is.EqualTo(DialogResult.OK), "A MennyiségSzerkesztés form DialogResult értéke nem OK.");
-            });
-        }
-
-        [Test]
-        public void Test_OpenModositasForm()
-        {
-            // Gomb lenyomása a fő formon
-            mainForm.Invoke(new Action(() =>
-            {
-                mainForm.button_change.PerformClick();
-            }));
-
-            // Megkeressük a megnyílt Módosítás formot
-            Módosítás modositasForm = null;
-            foreach (Form form in Application.OpenForms)
-            {
-                if (form is Módosítás && form.Visible)
-                {
-                    modositasForm = (Módosítás)form;
-                    break;
-                }
-            }
-
-            // Ellenőrizzük, hogy a Módosítás form bezáródott-e és DialogResult.OK lett-e
-            Assert.Multiple(() =>
-            {
-                Assert.That(modositasForm, Is.Not.Null, "A Módosítás form nem nyílt meg.");
-                Assert.That(modositasForm.DialogResult, Is.EqualTo(DialogResult.OK), "A Módosítás form DialogResult értéke nem OK.");
-            });
         }
     }
 }
